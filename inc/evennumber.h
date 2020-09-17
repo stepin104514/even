e189f982011fef177de68b21bcc64560cd3a68dd
stepin104514 <69117065+stepin104514@users.noreply.github.com>
2020-09-17 09:45:32 +0530

    Add files via upload

---
+++ inc/evennumber.h
@@ -0,0 +1,17 @@
+/**
+* @file evenNumber.h
+*
+*/
+#ifndef __EVENNUMBER_H__
+#define __EVENNNUMBER_H__
+
+/**
+* Checks whether an integer is a prime number or not
+* @param[in] number integer number to be checked
+* @return 1 if prime
+* @return -1 if not prime
+* @note Returns 0 for negative values, 0 and 1
+*/
+int evenNumber(int number);
+
+#endif /* #ifndef __EVENNUMBER_H__ */
---
+++ src/evennumber.c
@@ -0,0 +1,13 @@
+#include "evenNumber.h"
+
+int evenNumber(int n)
+{
+    if(n%2==0)
+	{
+	return 0;
+	}
+	else
+	{
+	return 1;
+	}
+}
