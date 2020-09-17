3bfb07d1f4d28cfb4a08b974b7ad5032f2a327c4
stepin104514 <69117065+stepin104514@users.noreply.github.com>
2020-09-17 10:16:02 +0530

    Add files via upload

---
+++ test/testeven.c
@@ -0,0 +1,56 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <primeNumber.h>
+#define PROJECT_NAME    "even"
+
+/* Prototypes for all the test functions */
+void test_not_even(void);
+void test_even(void);
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "not prime", test_not_prime);
+  CU_add_test(suite, "prime", test_prime);
+  CU_add_test(suite, "negative or zero", test_negative_zero);
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+void test_not_even(void) {
+  
+  CU_ASSERT(1 == evennumber(55));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == evennumber(13));
+}
+
+void test_even(void) {
+  CU_ASSERT(1 == evennumber(3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == evennumber(12));
+}
+
