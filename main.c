15c233529dac6b7f48f65c26fa14f7c0acc12c72
stepin104514 <69117065+stepin104514@users.noreply.github.com>
2020-09-17 09:52:27 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "evenNumber.h"
+
+int main(void)
+{
+	int n=7;
+	p = evenNumber(n);
+	switch(p)
+	{
+    case 1:
+        printf("odd");
+    case 0:
+        printf("even number");
+	}
+	return 0;
+}
